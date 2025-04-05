4e6fb335cded10cbb8a7ea505b706562e1da0bc7
현정우 <jejeje8@knou.ac.kr>
2025-04-05 16:16:31 +0900

    [Silver II] Title: DFS와 BFS, Time: 296 ms, Memory: 35360 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,41 +1,42 @@
 import sys
-input = sys.stdin.readline
+from collections import deque, defaultdict
 
-n, m, v = map(int, input().split())
-graph = [[] for _ in range(n+1)]
-visited = [False for _ in range(n+1)]
+read = sys.stdin.readline
 
-for _ in range(m):
-    s, e = map(int, input().split())
-    graph[s].append(e)
-    graph[e].append(s)
+# N: 정점의 개수, M: 간선의 개수, V: 시작할 정점 번호
+N, M, V = map(int, read().split())
 
-for row in graph:
-    row.sort()
+# 입력값 기반 그래프 생성
+graph = defaultdict(list)
+for _ in range(M):
+    v1, v2 = map(int, read().split())
+    # 양방향 이니까 반대도 입력
+    graph[v1].append(v2)
+    graph[v2].append(v1)
 
+# (단, 방문할 수 있는 정점이 여러 개인 경우 에는 정점 번호가 작은 것을 먼저 방문) <-- 이 조건 만족을 위한 정렬
+dfs_graph = {key: sorted(value, reverse=True) for key, value in graph.items()}
+bfs_graph = {key: sorted(value) for key, value in graph.items()}
 
-def DFS(now):
-    print(now, end=" ")
-    visited[now] = True
-    for next in graph[now]:
-        if not visited[next]:
-            DFS(next)
+def dfs(graph, start):
+    visit, visited = [start], []
 
+    while visit:
+        node = visit.pop()
+        if node not in visited:
+            visited.append(node)
+            visit.extend(graph.get(node, [])) # 정점만 있고 간선이 없는 경우 KeyError 방지, 그냥 graph[node] 를 하면 value 가 없어서 KeyError 발생
+    return visited
 
-def BFS(start):
-    visited[start] = True
-    que = [start]
+def bfs(graph, start):
+    visit, visited = deque([start]), []
 
-    while que:
-        now = que.pop(0)
-        print(now, end=" ")
-        for next in graph[now]:
-            if not visited[next]:
-                visited[next] = True
-                que.append(next)
+    while visit:
+        node = visit.popleft()
+        if node not in visited:
+            visited.append(node)
+            visit.extend(graph.get(node, [])) # 정점만 있고 간선이 없는 경우 KeyError 방지, 그냥 graph[node] 를 하면 value 가 없어서 KeyError 발생
+    return visited
 
-
-DFS(now=v)
-print()
-visited = [False for _ in range(n+1)]
-BFS(start=v)
+print(*dfs(dfs_graph, V))
+print(*bfs(bfs_graph, V))
