ac2bf521487e059fe214fc3a02e1af611e7d3a00
현정우 <jejeje8@knou.ac.kr>
2025-04-12 15:21:23 +0900

    [Silver V] Title: 단어 정렬, Time: 76 ms, Memory: 37532 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,10 +1,10 @@
-import sys, copy
+import sys
 
 read = sys.stdin.readline
 
 # 단어 중복 제거
 words = {read().rstrip() for _ in range(int(read()))}
 
-# 길이 정렬 후 길이 같으면 사전 정렬하여 출력
+# 길이 정렬 후 길이 같으면 사전 정렬 하여 출력
 for s in sorted(words, key=lambda x: (len(x), x)):
     print(s)
