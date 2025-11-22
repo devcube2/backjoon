767eb27c8f615ed20a7b8ae540050b416a449f31
현정우 <jejeje8@knou.ac.kr>
2025-11-22 23:02:36 +0900

    [level 1] Title: 실패율, Time: 5802.72 ms, Memory: 15.6 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,5 +1,5 @@
 def solution(N, stages):
-    stage_result = dict()
+    stage_result = []
     for stage_no in range(1, N + 1):
         # 스테이지 도전했으나 실패한 사람 수
         holder_count = stages.count(stage_no)
@@ -7,8 +7,9 @@ def solution(N, stages):
         challenger_count = len([stage for stage in stages if stage >= stage_no])
         # 스테이지와 해당 스테이지의 실패율 저장
         if challenger_count == 0:
-            stage_result[stage_no] = 0            
+            stage_result.append((stage_no, 0))
         else:
-            stage_result[stage_no] = holder_count / challenger_count
-    # 실패율 오름차순 정렬하고, 동일한 실패율 스페이지는 내림차순 정렬    
-    return sorted(stage_result, key=lambda x: (-stage_result[x], x))
+            stage_result.append((stage_no, holder_count / challenger_count))
+    # 실패율 오름차순 정렬하고, 동일한 실패율 스페이지는 내림차순 정렬
+    stage_result.sort(key=lambda item: (-item[1], item[0]))
+    return [result[0] for result in stage_result]
