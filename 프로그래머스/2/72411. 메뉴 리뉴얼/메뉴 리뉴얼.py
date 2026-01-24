118c5c8547c8510be3ac6ed9d3f233a731b63fcf
현정우 <jejeje8@knou.ac.kr>
2026-01-24 13:23:12 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 1.93 ms, Memory: 9.56 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,30 +1,15 @@
-from collections import Counter
-from itertools import combinations, takewhile
+import collections
+import itertools
 
 def solution(orders, course):
-    answer = []
-    
-    # 코스 주문별 정렬
-    orders = [sorted(order) for order in orders]
-    
-    # 코스별 순회
-    for course_count in course:
-        # 코스 조합 생성
-        courses = []
+    result = []
+
+    for course_size in course:
+        order_combinations = []
         for order in orders:
-            courses.extend(list(combinations(order, course_count)))
-        # 가장 많이 주문된 코스 추출
-        courses_count = Counter(courses).most_common()
-        most_courses = list(takewhile(lambda x: x[1] == courses_count[0][1], courses_count))
-        most_courses = [c[0] for c in most_courses]
-        
-        for most_course in most_courses:
-            # 손님별 2번 이상 주문된 코스만 추가
-            count = 0
-            for order in orders:
-                if set(most_course).issubset(order):
-                    count += 1
-                if count >= 2:
-                    answer.append(''.join(most_course))
-                    break
-    return sorted(answer)
+            order_combinations += itertools.combinations(sorted(order), course_size)
+
+        most_ordered = collections.Counter(order_combinations).most_common()
+        result += [ k for k, v in most_ordered if v > 1 and v == most_ordered[0][1] ]
+
+    return [ ''.join(v) for v in sorted(result) ]
