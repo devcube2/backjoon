c44337dcfba205bb065619f339cadfed8607b6c2
현정우 <jejeje8@knou.ac.kr>
2026-02-04 18:41:36 +0900

    [Silver I] Title: 그림, Time: 356 ms, Memory: 36872 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -5,14 +5,14 @@ read = sys.stdin.readline
 
 n, m = map(int, read().split())
 
-paper = [[int(c) for c in read().split()] for _ in range(n)]
+land = [[int(c) for c in read().split()] for _ in range(n)]
 
 dx = [0, 1, -1, 0, 0]
 dy = [0, 0, 0, -1, 1]
 
 
 # 탐색 하면서 색칠된 부분을 발견 하면 지우고 리턴 값에 1을 더한다. 리턴 값은 그림의 크기가 된다.
-def paint(x, y):
+def walk(x, y):
     count = 0
     queue = deque()
     queue.append((x, y))
@@ -25,9 +25,9 @@ def paint(x, y):
             if nx < 0 or nx >= m or ny < 0 or ny >= n:
                 continue
             # 색칠된 부분 이라면
-            if paper[ny][nx] == 1:
+            if land[ny][nx] == 1:
                 count += 1
-                paper[ny][nx] = 0
+                land[ny][nx] = 0
                 queue.append((nx, ny))
     return count
 
@@ -36,8 +36,8 @@ pictures = []
 # 모든 타일에 대해서 검사 한다.
 for y in range(n):
     for x in range(m):
-        if paper[y][x] == 1:
-            count = paint(x, y)
+        if land[y][x] == 1:
+            count = walk(x, y)
             if count > 0:
                 pictures.append(count)
 
